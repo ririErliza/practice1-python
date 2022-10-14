59bbd95e9a4c3b3269de62d195015584ff36362f
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-10-14 12:48:50 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-from ctypes import LittleEndianStructure
 
 
-# variables
+
+#-----------------------------------variables--------------------------------------
 
 name="Kiki"
 age=33
@@ -15,7 +15,7 @@ _name="Ryan"
 
 #----------------------------------------------------------------------------------
 
-#expressions and statements
+#--------------------------expressions and statements------------------------------
 
 
 
@@ -29,45 +29,115 @@ _name="Ryan"
 #statement is operation on a value
 
 name2="Rose"
-print(name2)
+#print(name2)
 
 #above can be written like this too :  name2="Rose" ; print(name2)  2 statements in one single line separated by semicolon
 
 #----------------------------------------------------------------------------------
 
-#comment
+#----------------------------------comment-----------------------------------------
 
 #using hash mark (#) to comment a code. everything after the hash mark is ignored.
 
-#indentation 
+#---------------------------------indentation--------------------------------------
 
-#indentation in python matters, has a meaning. everything indented belongs to a block like a control statement or conditional block or function or a class body
+#indentation in python matters, has a meaning. 
+# everything indented belongs to a block like a control statement or conditional block or function or a class body
 
 
 #----------------------------------------------------------------------------------
 
-#Data Types
+#-------------------------------Data Types-----------------------------------------
 
 #string
 name3="Beau"
 
 #checking data types method 1
-print(type(name3)) #will return <class 'str'> at the console
+#print(type(name3)) #will return <class 'str'> at the console
 
 #checking data types method 2 (by comparing it)
-print(type(name3)== str) #will return True at the console
+#print(type(name3)== str) #will return True at the console
 
 #or
-print(isinstance(name,str)) #will return True at the console
+#print(isinstance(name,str)) #will return True at the console
 
 
 
 #number
 amount1=2
-print(isinstance(amount1,int)) #will return True at the console
-print(isinstance(amount1,float)) #will return False
-print(isinstance(amount1,str)) #will return False
+#print(isinstance(amount1,int)) #will return True at the console
+#print(isinstance(amount1,float)) #will return False
+#print(isinstance(amount1,str)) #will return False
 
 amount2=2.9
-print(isinstance(amount2,float)) #will return True
+#print(isinstance(amount2,float)) #will return True
 
+amount3=float(2)  #this here is converting the integer to float
+#print(isinstance(amount3,float)) #will return True
+
+amount4=int("20") #this here is converting the string to integer
+#print(isinstance(amount4,str)) #will return False
+#print(isinstance(amount4,int)) #will return True
+
+#converting str to number vv (casting)
+number="23"
+numberToInt = int(number)
+print(isinstance(numberToInt,int)) #will return True
+
+number1="test"
+numberToInt1 = int(number1)
+print(isinstance(numberToInt1,int)) #will return error
+
+
+
+#other data types
+
+# complex for complex numbers
+# bool for booleans
+# list for lists
+# tuple for tuples
+# range for ranges
+# dict for dictionaries
+# set for sets
+
+
+
+#----------------------------------------------------------------------------------
+
+
+#-------------------------------OPERATORS------------------------------------------
+
+
+#--------------Arithmetic Operators-----------------
+
+#--------------Comparison Operators-----------------
+
+#--------------Comparison Operators-----------------
+
+#--------------Boolean Operators--------------------
+
+#--------------Bitwise Operators--------------------
+
+#--------------is & in Operators--------------------
+
+#--------------Ternary Operator---------------------
+
+
+#----------------------------------------------------------------------------------
+
+#--------------------------------Strings-------------------------------------------
+
+#--------------String Methods---------------------
+
+#--------------Escaping Characters----------------
+
+#--------------String Characters & Slicing--------
+
+
+#----------------------------------------------------------------------------------
+
+#--------------------------------Booleans------------------------------------------
+
+#----------------------------------------------------------------------------------
+
+#--------------------------------Number Data Types---------------------------------
