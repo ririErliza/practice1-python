ea470d1ea534ab57f6bceb795e5db83b69af4afe
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-10-28 13:25:14 +0200

    update

--- test.py
+++ test.py
@@ -1231,14 +1231,22 @@ print(factorial(3)) # 6
 def logtime(func):
     def wrapper():
         #do something before
+        print("before")
         val= func()
         # do something after
+        print("after")
         return val
     return wrapper
 
 @logtime
 def hello():
     print("hello")
+
+hello()
+
+# before
+# hello
+# after
 
 #----------------------------------------------------------------------------------
 
